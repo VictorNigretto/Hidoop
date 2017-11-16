93a99f37c31082299887af49a2913b5092ca4a8e
Bor3alis <marianessia@gmail.com>
2017-11-16 19:51:08 +0100

    petites modifs

--- HDFS_V0/src/CreateKVfile.java
+++ HDFS_V0/src/CreateKVfile.java
@@ -8,11 +8,11 @@ import java.rmi.RemoteException;
 public class CreateKVfile {
 
     public static void main (String[] args){
-        File f = new File("test.kv");
+        File f = new File("test.kvread");
         KV kv1 = new KV("premier","1");
         KV kv2 = new KV("deuxieme","2");
         try {
-            FileOutputStream fos = new FileOutputStream(f);
+            /*FileOutputStream fos = new FileOutputStream(f);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
 
@@ -20,14 +20,15 @@ public class CreateKVfile {
             oos.writeObject(kv2);
 
             oos.close();
-            fos.close();
+            fos.close();*/
 
             FileInputStream fis = new FileInputStream(f);
             ObjectInputStream ois = new ObjectInputStream(fis);
             try {
                 KV newkv1 = (KV) ois.readObject();
-                KV newkv2 = (KV) ois.readObject();
                 System.out.println(newkv1.toString());
+
+                KV newkv2 = (KV) ois.readObject();
                 System.out.println(newkv2.toString());
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
