8aa85d8e49a09e8e286f349738af0569ac92fd17
Bor3alis <marianessia@gmail.com>
2017-11-23 10:17:12 +0100

    ajout de tests

---
+++ HDFS_V0/src/Tests/HdfsClientTest.java
@@ -0,0 +1,36 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HdfsClientTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void hdfsDelete() throws Exception {
+
+    }
+
+    @Test
+    public void hdfsWrite() throws Exception {
+    }
+
+    @Test
+    public void hdfsRead() throws Exception {
+    }
+
+    @Test
+    public void main() throws Exception {
+    }
+
+}
