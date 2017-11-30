22a8ecbde9e93ab96a40a54f94f6b45da4e5bc13
Medard Marine <mmedard@localhost.localdomain>
2017-11-30 10:00:09 +0100

    petit ajout de fichiers

--- HDFS_V0/src/hdfs/Test.java
+++ HDFS_V0/src/hdfs/Test.java
@@ -9,6 +9,7 @@ import java.net.UnknownHostException;
 
 import formats.Format;
 import formats.Format.Type;
+import formats.FormatKV;
 import formats.KV;
 import formats.FormatLine;
 import formats.Format.Commande;
@@ -19,44 +20,57 @@ public class Test {
 	public static void main(String[] args) {
 		
 
-	/*	FormatLine lf = new FormatLine("test.txt",Type.LINE);
+		FormatLine lf = new FormatLine("test.txt");
 
 		lf.open(Format.OpenMode.R);
 		KV kv = lf.read();
-		System.out.println(kv.v);
-		System.out.println(kv.k);
+		System.out.println(kv);
 		
-		//lf.open(Format.OpenMode.R);
 		kv = lf.read();
-		System.out.println(kv.v);
-		System.out.println(kv.k);
+		System.out.println(kv);
+		
+		kv = lf.read();
+		System.out.println(kv);
+		
+		lf.close();
+		lf.setFname("testWrite.txt");
 	
-		//lf.open(Format.OpenMode.R);
-		kv = lf.read();
-		System.out.println(kv.v);
-		System.out.println(kv.k);
 		
 		kv = new KV("Cours","Math");
 		lf.open(Format.OpenMode.W);
 		lf.write(kv);
 		
 		kv = new KV("Prof","Ellips");
-		//lf.open(Format.OpenMode.W);
-		lf.write(kv);*/
-		File f = new File("data/filesample.txt");
-		
-		System.out.println("AAA");
-		
-		try {
-			f.createNewFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		lf.write(kv);
 
+		lf.close();
 
 		
+		
+		FormatKV lk = new FormatKV("testKV.txt");
 
+		lk.open(Format.OpenMode.R);
+		kv = lk.read();
+		System.out.println(kv);
+		
+		kv = lk.read();
+		System.out.println(kv);
+		
+		kv = lk.read();
+		System.out.println(kv);
+		
+		lf.close();
+		lk.setFname("testKVWrite.txt");
+	
+		
+		kv = new KV("Cours","Math");
+		lk.open(Format.OpenMode.W);
+		lk.write(kv);
+		
+		kv = new KV("Prof","Ellips");
+		lk.write(kv);
+
+		lk.close();
 
 	}
 }
