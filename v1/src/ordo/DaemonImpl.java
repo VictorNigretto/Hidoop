e5aa40b5b6d4e7d1bd3e814f0a17b1bef389e60a
Medard Marine <mmedard@localhost.localdomain>
2018-01-04 00:45:33 +0100

    encore un commit

--- v1/src/ordo/DaemonImpl.java
+++ v1/src/ordo/DaemonImpl.java
@@ -2,24 +2,46 @@ package ordo;
 
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import formats.Format;
+import hdfs.Machine;
+import hdfs.NameNode;
 import map.Mapper;
 
 public class DaemonImpl extends UnicastRemoteObject implements Daemon {
 
 	private static final long serialVersionUID = 1L;
 	
-	private String name; // Les démons ont un nom pour qu'on puisse les différencier
+	static private String name; // Les démons ont un nom pour qu'on puisse les différencier
+	private Machine machine;
 	
-	protected DaemonImpl(String name) throws RemoteException {
+	
+	protected DaemonImpl(String nomDaemon, int port, String name ) throws RemoteException {
 		super();
-		this.name = name;
+		this.name = nomDaemon;
+		this.machine = new Machine(name, port, nomDaemon);
 		System.out.println("Création du Deamon " + this.name);
+		
+        try {
+        	
+			machine.setNom(InetAddress.getLocalHost().getHostName());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Machine getMachine() {
+		return machine;
+	}
+
+	public void setMachine(Machine machine) {
+		this.machine = machine;
 	}
 
 	@Override
@@ -29,6 +51,8 @@ public class DaemonImpl extends UnicastRemoteObject implements Daemon {
 		// On ouvre le formats sur le démons, pour récupérer les chunks
 		reader.open(Format.OpenMode.R);
 		writer.open(Format.OpenMode.W);
+		
+		
 
 		System.out.println("Lancement du Map ...");
 		m.map(reader, writer);
@@ -47,15 +71,20 @@ public class DaemonImpl extends UnicastRemoteObject implements Daemon {
 	// Le premier paramètre sera le nom du démon
 	public static void main(String args[]) {
 		try {
-			Daemon d = new DaemonImpl(args[0]);
+			
+			Daemon d = new DaemonImpl(args[0], Integer.parseInt(args[1]), args[2]);
 			// On l'enregistre auprès du serveur de nom, qu'il faudra avoir lancé au préalable !
-            String nomMachine = InetAddress.getLocalHost().getHostName();
             //Naming.rebind("//" + "localhost/" + ((DaemonImpl) d).getName(), d);
             //Registry registry = LocateRegistry.createRegistry(1099);
             //registry.rebind("//localhost:1099",  d);
             System.out.println("//localhost:1199/" + ((DaemonImpl) d).getName());
             Naming.rebind("//localhost:1199/" + ((DaemonImpl) d).getName(), d);
             System.out.println("Done !");
+            
+            RessourceManager rm = ((RessourceManager) Naming.lookup("//localhost:1199/" + "RessourceManager" ));
+            while (true) {
+            	rm.DemonFonctionne(name);
+            }
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -69,5 +98,8 @@ public class DaemonImpl extends UnicastRemoteObject implements Daemon {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+
 
 }
